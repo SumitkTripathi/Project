62a4d423620a1efa76ebba02bf942e6c2d434f8b
Sumit <64913575+SumitkTripathi@users.noreply.github.com>
2024-09-17 09:33:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def predict():
     prediction = model.predict(pd.DataFrame([[car_model,company,year,kilometer,fuel_type]],columns=["name","company","year","kms_driven","fuel_type"]))
     return str(np.round(prediction[0],2))
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
